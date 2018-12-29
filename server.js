8e99bb17260ec549773706b45f4f9067b7667445
jjblues86 <jeromejoof@yahoo.com>
2018-12-29 11:28:50 -0800

    feature 1

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000
 
 // Parse request.body
 app.use(express.urlencoded({extended: true}))
+app.use(express.static('./public'));
 
 // Database Setup
 const client = new pg.Client('postgres://localhost:5432/books_app')
@@ -21,8 +22,27 @@ app.set('view engine', 'ejs')
 // Routes
 // Home Route
 app.get('/', (req, res) => {
-  res.render('pages/index')
-})
+  res.render('pages/index', {
+    hello: 'World'
+  });
+});
+
+
+// Search Route
+app.post('/searches', search)
+
+function search(req, res) {
+  let searchStr = req.body.search[0]
+  let searchStr = req.body.search[1]
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=search'
+
+  // Search Type conditionals
+  if(searchType === 'title') {
+    url += `+intitle:${searchStr}`
+  } else if (searchStr === 'author') {
+    url += `inauthor:${searchStr}`
+  }
+}
 
 // Localhost listener
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
