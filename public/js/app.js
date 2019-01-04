6b5725812494d2d65a334e8ca51d08da643d0fe2
jjblues86 <jeromejoof@yahoo.com>
2019-01-03 21:30:20 -0800

    working on updating the form

--- public/js/app.js
+++ public/js/app.js
@@ -3,5 +3,15 @@
 $(document).ready(function() {
 	$("body").on('click', '.top', function() {
 		$("nav.menu").toggleClass("menu_show");
-	});
+  });
+  
+  $('form').hide();
+  $('#show-form').on('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    console.log($(e.target).parent());
+    $(e.target).parent().hide();
+    $('form').show();
+  })
 });
